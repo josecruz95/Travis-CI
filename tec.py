0e625f031e1629abc12120e7f79fb10d9af6d2f3
josecruz95m <josecruz95@hotmail.com>
2018-09-07 18:08:14 -0500

    [ADD]tec.py test in the file

--- tec.py
+++ tec.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-suma===4+6
+suma=4+6
 
-def resultado(suma)
-	if suma == 10
+def resultado(suma):
+	if suma == 10:
 		print('''La suma es correcta es %s''') % str(suma)
 	else:
 		print('''La suma no es igual a %s''') % string(suma)
+
+def test_suma():	
+	assert resultado(10) != 10
 	
