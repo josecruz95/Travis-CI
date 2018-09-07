002ba9d3424c87b5adc9d553e98d7688c6600df7
josecruz95m <josecruz95@hotmail.com>
2018-09-07 17:49:50 -0500

    Cambio de sintaxis

--- tec.py
+++ tec.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-suma=4+6
+suma===4+6
 
-def resultado(suma):
-	if suma == 10:
-		print('La suma es correcta es %s') % str(suma)
+def resultado(suma)
+	if suma == 10
+		print('''La suma es correcta es %s''') % str(suma)
 	else:
-		print('La suma no es igual a %s') % str(suma)
-	return suma
+		print('''La suma no es igual a %s''') % string(suma)
+	
